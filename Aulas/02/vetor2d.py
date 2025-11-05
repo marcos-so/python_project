44afa58a95f915454f31a353156c00d501bdce42
Marcos Vinícius <105286480+marcos-so@users.noreply.github.com>
2025-11-05 11:27:40 -0300

    feat: aula 02

---
+++ Aulas/02/vetor2d.py
@@ -0,0 +1,9 @@
+class Vetor2D:
+
+	def __init__(self, x:int, y:int) -> None:
+		...
+	def __add__(self, other): pass
+	def __sub__(self, other): pass
+	def __mul__(self, other): pass
+	def __truediv__(self, other): pass
+	def __repr__(self,other): pass
